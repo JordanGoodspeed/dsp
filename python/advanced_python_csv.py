61b1ca1ca80a8a241b98c057dbf7e59c83e0f309
Jordan Goodspeed <jordangoodspeed@gmail.com>
2015-12-26 17:41:53 -0500

    Completed writing email addresses to csv.

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+import csv
+
+emails = []
+
+with open('faculty.csv', 'rb') as f:
+	reader = csv.reader(f, delimiter=",")
+	reader.next()
+	for row in reader:
+		emails.append(row[3])
+
+with open('emails.csv', 'wb') as f:
+	writer = csv.writer(f, dialect='excel')
+	for item in emails:
+		writer.writerow([item])
